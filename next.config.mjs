c5733a636914af49f15d8b1c364b48043a1d3267
nimba005 <nimbaoyugi@gmail.com>
2024-09-25 23:34:53 +0300

    Redeployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: isProd ? '/expense-tracker' : '',
+  assetPrefix: isProd ? '/expense-tracker/' : '',
+};
 
 export default nextConfig;
-
